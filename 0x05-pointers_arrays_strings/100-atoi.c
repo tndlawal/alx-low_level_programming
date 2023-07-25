8e0003fd7e34d6a2d21e02c3b3e046f22aa137be
tndlawal <tndlawal@gmail.com>
2023-07-24 17:27:23 -0700

    define 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,20 +1,13 @@
 #include "main.h"
 
-
 /**
-
  * _atoi - converts a string to an integer
-
  * @s: string to be converted
-
  *
-
  * Return: the int converted from the string
-
  */
 
 int _atoi(char *s)
-
 {
 	int i, d, n, len, f, digit;
 
